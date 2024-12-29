fc2366f5e3a61124c5b5cb360dffe474872cf05a
AdolfoNalin <nalinadolfo33@gmail.com>
2024-12-29 20:18:50 -0300

    Criando as configurações para o consumo da API

--- FrontConfin/Program.cs
+++ FrontConfin/Program.cs
@@ -1,10 +1,15 @@
 using FrontConfin.View;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using System.DirectoryServices.ActiveDirectory;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 
 namespace FrontConfin
 {
     internal static class Program
     {
+        public static IConfiguration Configuration;
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -13,8 +18,11 @@ namespace FrontConfin
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
+
             ApplicationConfiguration.Initialize();
-            Application.Run(new frmMenu());
+            Application.Run(new frmLogin());
         }
     }
 }
