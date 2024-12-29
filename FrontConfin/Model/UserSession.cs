563a220984a0dfeb169ab780af4fb7f4243a821f
AdolfoNalin <nalinadolfo33@gmail.com>
2024-12-29 20:22:24 -0300

    Criando as entidades: UserResponse, UserRequest e UserSession

---
+++ FrontConfin/Model/UserSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontConfin.Model
+{
+    public static class UserSession 
+    {
+        public static Guid Id { get; set; }
+        public static string Name { get; set; }
+        public static string Login { get; set; }
+        public static string Function { get; set; }
+    }
+}
