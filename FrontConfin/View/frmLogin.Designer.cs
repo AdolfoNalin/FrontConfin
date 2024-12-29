09dc2af41db2179d789ebb934e1a962ecef758ba
AdolfoNalin <nalinadolfo33@gmail.com>
2024-12-29 20:36:15 -0300

    Criando a tela login e o método de login

---
+++ FrontConfin/View/frmLogin.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace FrontConfin.View
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtLogin = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            txtPassword = new TextBox();
+            btnClose = new Button();
+            btnLogin = new Button();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // txtLogin
+            // 
+            txtLogin.Location = new Point(201, 39);
+            txtLogin.Name = "txtLogin";
+            txtLogin.Size = new Size(183, 23);
+            txtLogin.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(158, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Login";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(158, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Senha";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(201, 83);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '#';
+            txtPassword.Size = new Size(183, 23);
+            txtPassword.TabIndex = 2;
+            // 
+            // btnClose
+            // 
+            btnClose.Image = Properties.Resources.Cancelar32X32;
+            btnClose.ImageAlign = ContentAlignment.TopLeft;
+            btnClose.Location = new Point(108, 146);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(102, 44);
+            btnClose.TabIndex = 4;
+            btnClose.Text = "Cancelar ";
+            btnClose.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Image = Properties.Resources.Confirmar32X32;
+            btnLogin.ImageAlign = ContentAlignment.TopLeft;
+            btnLogin.Location = new Point(216, 146);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(95, 44);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Logar";
+            btnLogin.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(22, 29);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(105, 111);
+            panel1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.UserAvatar;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(105, 111);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(406, 217);
+            Controls.Add(panel1);
+            Controls.Add(btnLogin);
+            Controls.Add(btnClose);
+            Controls.Add(label2);
+            Controls.Add(txtPassword);
+            Controls.Add(label1);
+            Controls.Add(txtLogin);
+            KeyPreview = true;
+            MaximizeBox = false;
+            Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            KeyDown += frmLogin_KeyDown;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtLogin;
+        private Label label1;
+        private Label label2;
+        private TextBox txtPassword;
+        private Button btnClose;
+        private Button btnLogin;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ FrontConfin/View/frmLogin.cs
@@ -0,0 +1,68 @@
+﻿using FrontConfin.Model;
+using FrontConfin.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrontConfin.View
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        #region btnClose
+        private async void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        #endregion
+
+        #region btnLogin
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UserResquest login = new UserResquest()
+                {
+                    Login = txtLogin.Text,
+                    Password = txtPassword.Text
+                };
+
+                UserResponse response = await UserService.Login(login);
+
+                UserSession.Id = response.User.Id;
+                UserSession.Name = response.User.Name;
+                UserSession.Login = response.User.Login;
+                UserSession.Function = response.User.Function;
+
+                frmMenu screen = new frmMenu();
+                this.Hide();
+                screen.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion
+
+        #region frmLogin_KeyDown
+        private void frmLogin_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnLogin_Click(sender, e);
+            }
+        }
+        #endregion
+    }
+}
