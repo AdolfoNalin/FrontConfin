f564656d6ee05bd9376a05148d5057c5d6da023b
AdolfoNalin <nalinadolfo33@gmail.com>
2025-01-31 21:57:15 -0300

    Criando modelo response de paginação

---
+++ FrontConfin/Model/PaginationResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontConfin.Model
+{
+    public class PaginationResponse<T> where T : class
+    {
+        public IEnumerable<T> Data { get; set; }
+        public long AllLine { get; set; }
+        public int Skip { get; set; }
+        public int Take { get; set; }
+
+        public PaginationResponse(IEnumerable<T> data, long allLine, int skip, int take)
+        {
+            Data = data;
+            AllLine = allLine;
+            Skip = skip;
+            Take = take;
+        }
+    }
+}
