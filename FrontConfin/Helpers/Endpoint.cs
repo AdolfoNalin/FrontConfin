62671b9be717a830fd322dd8efe86aab4dbecef6
AdolfoNalin <nalinadolfo33@gmail.com>
2025-02-02 20:03:05 -0300

    Criando Função GetEndpoint
    Função que retorna o endereço do banco de dados

---
+++ FrontConfin/Helpers/Endpoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FrontConfin.Helpers
+{
+    public class Endpoint
+    {
+        #region Endpoint
+        public static string GetEndpoint()
+        {
+            try
+            {
+                string endpoint = Program.Configuration.GetSection("WFConfin:Endpoint").Value ?? throw new ArgumentNullException("Endpoint não encontrado!");
+                return endpoint;
+            }
+            catch(ArgumentNullException ae)
+            {
+                MessageBox.Show(ae.Message);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+        #endregion
+    }
+}
