d77571c130276a367db1fa7768ce6c2f6ca8a4ce
AdolfoNalin <nalinadolfo33@gmail.com>
2025-02-19 21:08:53 -0300

    Criando funções Post e GetAll

--- FrontConfin/Service/UserService.cs
+++ FrontConfin/Service/UserService.cs
@@ -27,12 +27,13 @@ namespace FrontConfin.Service
 					throw new ArgumentNullException("Login ou senha estão vazio");
 				}
 
-				var endpoint = Program.Configuration.GetSection("WFConfin:Endpoint").Value;
+				var endpoint = Helpers.Endpoint.GetEndpoint();
 
 				HttpClient client = new HttpClient();
 				client.BaseAddress = new Uri(endpoint);
 				client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 				client.Timeout = new TimeSpan(0, 0, 30);
+
 				HttpResponseMessage response = await client.PostAsJsonAsync("User/Login", user);
 
 				if (response.StatusCode == HttpStatusCode.OK)
@@ -57,6 +58,69 @@ namespace FrontConfin.Service
 				return null;
 			}
         }
+        #endregion
+
+        #region GetAll
+		public async Task<List<User>> GetAll()
+		{
+			List<User> list = new List<User>();
+			try
+			{
+				HttpClient client = Helpers.ClientHttp.CreateHttpClient();
+				HttpResponseMessage response = await client.GetAsync("User");
+
+				if(response.IsSuccessStatusCode)
+				{
+					list = JsonConvert.DeserializeObject<List<User>>(await response.Content.ReadAsStringAsync() ?? throw new ArgumentNullException("Lista está vazia!"));
+				}
+				else
+				{
+					string message = await response.Content.ReadAsStringAsync();
+					MessageBox.Show(message);
+				}
+
+				return list;
+			}
+			catch(ArgumentNullException ae)
+			{
+				MessageBox.Show(ae.Message);
+				return null;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				return null;
+			}
+		}
+        #endregion
+
+        #region Post
+		public async Task<bool> Post(User user)
+		{
+			bool value = false;
+			try
+			{
+				HttpClient client = Helpers.ClientHttp.CreateHttpClient();
+				HttpResponseMessage response = await client.PostAsJsonAsync("User", user);
+
+				if(response.IsSuccessStatusCode)
+				{
+					value = true;
+					MessageBox.Show(await response.Content.ReadAsStringAsync());
+				}
+				else
+				{
+					MessageBox.Show(await response.Content.ReadAsStringAsync());
+				}
+
+				return value;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				return false;
+			}
+		}
         #endregion
     }
 }
