f52e4a6524833cb29a322588c45e29339b5f46e2
AdolfoNalin <nalinadolfo33@gmail.com>
2025-02-19 21:23:15 -0300

    Criando tela Cidade

---
+++ FrontConfin/View/frmCity.Designer.cs
@@ -0,0 +1,515 @@
+﻿namespace FrontConfin.View
+{
+    partial class frmCity
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TabControl1 = new TabControl();
+            tpRegistration = new TabPage();
+            dgCity = new DataGridView();
+            tpDatails = new TabPage();
+            txtId = new TextBox();
+            lblId = new Label();
+            cbState = new ComboBox();
+            txtName = new TextBox();
+            lblName = new Label();
+            btnAcronym = new Label();
+            panel5 = new Panel();
+            panel2 = new Panel();
+            cbDesc = new CheckBox();
+            btnSearch = new Button();
+            txtSearch = new TextBox();
+            panel1 = new Panel();
+            txtSkip = new TextBox();
+            label2 = new Label();
+            txtTake = new TextBox();
+            label1 = new Label();
+            btnEnd = new Button();
+            btnNext = new Button();
+            btnPrevious = new Button();
+            btnPrimary = new Button();
+            panel4 = new Panel();
+            btnExit = new Button();
+            panel3 = new Panel();
+            panel7 = new Panel();
+            btnCancel = new Button();
+            btnSave = new Button();
+            panel6 = new Panel();
+            btnDelete = new Button();
+            btnUpdate = new Button();
+            btnNew = new Button();
+            TabControl1.SuspendLayout();
+            tpRegistration.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgCity).BeginInit();
+            tpDatails.SuspendLayout();
+            panel5.SuspendLayout();
+            panel2.SuspendLayout();
+            panel1.SuspendLayout();
+            panel4.SuspendLayout();
+            panel3.SuspendLayout();
+            panel7.SuspendLayout();
+            panel6.SuspendLayout();
+            SuspendLayout();
+            // 
+            // TabControl1
+            // 
+            TabControl1.Alignment = TabAlignment.Left;
+            TabControl1.Controls.Add(tpRegistration);
+            TabControl1.Controls.Add(tpDatails);
+            TabControl1.Dock = DockStyle.Fill;
+            TabControl1.ImeMode = ImeMode.NoControl;
+            TabControl1.Location = new Point(0, 100);
+            TabControl1.Multiline = true;
+            TabControl1.Name = "TabControl1";
+            TabControl1.SelectedIndex = 0;
+            TabControl1.Size = new Size(955, 288);
+            TabControl1.TabIndex = 7;
+            // 
+            // tpRegistration
+            // 
+            tpRegistration.Controls.Add(dgCity);
+            tpRegistration.Location = new Point(27, 4);
+            tpRegistration.Name = "tpRegistration";
+            tpRegistration.Padding = new Padding(3);
+            tpRegistration.Size = new Size(924, 280);
+            tpRegistration.TabIndex = 0;
+            tpRegistration.Text = "Registros";
+            tpRegistration.UseVisualStyleBackColor = true;
+            // 
+            // dgCity
+            // 
+            dgCity.BackgroundColor = SystemColors.ActiveCaption;
+            dgCity.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgCity.Dock = DockStyle.Fill;
+            dgCity.Location = new Point(3, 3);
+            dgCity.Name = "dgCity";
+            dgCity.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgCity.Size = new Size(918, 274);
+            dgCity.TabIndex = 0;
+            dgCity.SelectionChanged += dgCity_SelectionChanged;
+            // 
+            // tpDatails
+            // 
+            tpDatails.Controls.Add(txtId);
+            tpDatails.Controls.Add(lblId);
+            tpDatails.Controls.Add(cbState);
+            tpDatails.Controls.Add(txtName);
+            tpDatails.Controls.Add(lblName);
+            tpDatails.Controls.Add(btnAcronym);
+            tpDatails.Location = new Point(27, 4);
+            tpDatails.Name = "tpDatails";
+            tpDatails.Padding = new Padding(3);
+            tpDatails.Size = new Size(924, 280);
+            tpDatails.TabIndex = 1;
+            tpDatails.Text = "Detalhes";
+            tpDatails.UseVisualStyleBackColor = true;
+            // 
+            // txtId
+            // 
+            txtId.CharacterCasing = CharacterCasing.Upper;
+            txtId.Location = new Point(30, 43);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(288, 23);
+            txtId.TabIndex = 6;
+            // 
+            // lblId
+            // 
+            lblId.AutoSize = true;
+            lblId.Location = new Point(33, 26);
+            lblId.Name = "lblId";
+            lblId.Size = new Size(17, 15);
+            lblId.TabIndex = 5;
+            lblId.Text = "Id";
+            // 
+            // cbState
+            // 
+            cbState.FormattingEnabled = true;
+            cbState.Location = new Point(27, 97);
+            cbState.Name = "cbState";
+            cbState.Size = new Size(46, 23);
+            cbState.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            txtName.CharacterCasing = CharacterCasing.Upper;
+            txtName.Location = new Point(96, 97);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(222, 23);
+            txtName.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Location = new Point(99, 80);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(43, 15);
+            lblName.TabIndex = 2;
+            lblName.Text = "Nome:";
+            // 
+            // btnAcronym
+            // 
+            btnAcronym.AutoSize = true;
+            btnAcronym.Location = new Point(30, 79);
+            btnAcronym.Name = "btnAcronym";
+            btnAcronym.Size = new Size(35, 15);
+            btnAcronym.TabIndex = 0;
+            btnAcronym.Text = "Sigla:";
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(panel2);
+            panel5.Controls.Add(panel1);
+            panel5.Controls.Add(panel4);
+            panel5.Dock = DockStyle.Top;
+            panel5.Location = new Point(0, 0);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(955, 100);
+            panel5.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(cbDesc);
+            panel2.Controls.Add(btnSearch);
+            panel2.Controls.Add(txtSearch);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(445, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(351, 100);
+            panel2.TabIndex = 5;
+            // 
+            // cbDesc
+            // 
+            cbDesc.AutoSize = true;
+            cbDesc.Location = new Point(15, 78);
+            cbDesc.Name = "cbDesc";
+            cbDesc.Size = new Size(89, 19);
+            cbDesc.TabIndex = 7;
+            cbDesc.Text = "Descresente";
+            cbDesc.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Image = Properties.Resources.Lupa32X32;
+            btnSearch.ImageAlign = ContentAlignment.TopLeft;
+            btnSearch.Location = new Point(224, 18);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(99, 45);
+            btnSearch.TabIndex = 6;
+            btnSearch.Text = "Pesquisar";
+            btnSearch.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Location = new Point(15, 30);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(192, 23);
+            txtSearch.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(txtSkip);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(txtTake);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(btnEnd);
+            panel1.Controls.Add(btnNext);
+            panel1.Controls.Add(btnPrevious);
+            panel1.Controls.Add(btnPrimary);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(445, 100);
+            panel1.TabIndex = 0;
+            // 
+            // txtSkip
+            // 
+            txtSkip.Location = new Point(254, 68);
+            txtSkip.Name = "txtSkip";
+            txtSkip.Size = new Size(28, 23);
+            txtSkip.TabIndex = 9;
+            txtSkip.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(205, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Página";
+            // 
+            // txtTake
+            // 
+            txtTake.Location = new Point(154, 68);
+            txtTake.Name = "txtTake";
+            txtTake.Size = new Size(30, 23);
+            txtTake.TabIndex = 7;
+            txtTake.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(33, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Registros por Página";
+            // 
+            // btnEnd
+            // 
+            btnEnd.Image = Properties.Resources.SetaUltima32X32;
+            btnEnd.ImageAlign = ContentAlignment.MiddleRight;
+            btnEnd.Location = new Point(337, 12);
+            btnEnd.Name = "btnEnd";
+            btnEnd.Size = new Size(103, 45);
+            btnEnd.TabIndex = 4;
+            btnEnd.Text = "Ultima";
+            btnEnd.TextImageRelation = TextImageRelation.TextBeforeImage;
+            btnEnd.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            btnNext.Image = Properties.Resources.Setaproxima32X32;
+            btnNext.ImageAlign = ContentAlignment.TopRight;
+            btnNext.Location = new Point(230, 12);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(103, 45);
+            btnNext.TabIndex = 3;
+            btnNext.Text = "Próxima";
+            btnNext.TextImageRelation = TextImageRelation.TextBeforeImage;
+            btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnPrevious
+            // 
+            btnPrevious.Image = Properties.Resources.SetaAnterior32X32;
+            btnPrevious.ImageAlign = ContentAlignment.TopLeft;
+            btnPrevious.Location = new Point(123, 12);
+            btnPrevious.Name = "btnPrevious";
+            btnPrevious.Size = new Size(103, 45);
+            btnPrevious.TabIndex = 2;
+            btnPrevious.Text = "Anterior";
+            btnPrevious.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnPrevious.UseVisualStyleBackColor = true;
+            // 
+            // btnPrimary
+            // 
+            btnPrimary.Image = Properties.Resources.SetaPrimeira32X32;
+            btnPrimary.ImageAlign = ContentAlignment.TopLeft;
+            btnPrimary.Location = new Point(19, 12);
+            btnPrimary.Name = "btnPrimary";
+            btnPrimary.Size = new Size(103, 45);
+            btnPrimary.TabIndex = 1;
+            btnPrimary.Text = "Primeiro";
+            btnPrimary.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnPrimary.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(btnExit);
+            panel4.Dock = DockStyle.Right;
+            panel4.Location = new Point(796, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(159, 100);
+            panel4.TabIndex = 4;
+            // 
+            // btnExit
+            // 
+            btnExit.Image = Properties.Resources.Sair32X321;
+            btnExit.ImageAlign = ContentAlignment.TopLeft;
+            btnExit.Location = new Point(66, 8);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(85, 45);
+            btnExit.TabIndex = 5;
+            btnExit.Text = "Sair";
+            btnExit.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(panel7);
+            panel3.Controls.Add(panel6);
+            panel3.Dock = DockStyle.Bottom;
+            panel3.Location = new Point(0, 388);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(955, 62);
+            panel3.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            panel7.Controls.Add(btnCancel);
+            panel7.Controls.Add(btnSave);
+            panel7.Dock = DockStyle.Right;
+            panel7.Location = new Point(703, 0);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(252, 62);
+            panel7.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Image = Properties.Resources.Cancelar32X32;
+            btnCancel.ImageAlign = ContentAlignment.TopLeft;
+            btnCancel.Location = new Point(112, 6);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(103, 45);
+            btnCancel.TabIndex = 5;
+            btnCancel.Text = "Calcelar";
+            btnCancel.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Image = Properties.Resources.Salvar32X32;
+            btnSave.ImageAlign = ContentAlignment.TopLeft;
+            btnSave.Location = new Point(3, 6);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(103, 45);
+            btnSave.TabIndex = 4;
+            btnSave.Text = "Salvar";
+            btnSave.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(btnDelete);
+            panel6.Controls.Add(btnUpdate);
+            panel6.Controls.Add(btnNew);
+            panel6.Dock = DockStyle.Left;
+            panel6.Location = new Point(0, 0);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(365, 62);
+            panel6.TabIndex = 0;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Image = Properties.Resources.Excluir32X32;
+            btnDelete.ImageAlign = ContentAlignment.TopLeft;
+            btnDelete.Location = new Point(244, 5);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(103, 45);
+            btnDelete.TabIndex = 4;
+            btnDelete.Text = "Excluir";
+            btnDelete.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Image = Properties.Resources.Editar32X32;
+            btnUpdate.ImageAlign = ContentAlignment.TopLeft;
+            btnUpdate.Location = new Point(135, 6);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(103, 45);
+            btnUpdate.TabIndex = 3;
+            btnUpdate.Text = "Alterar";
+            btnUpdate.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnNew
+            // 
+            btnNew.Image = Properties.Resources.Novo32X32;
+            btnNew.ImageAlign = ContentAlignment.TopLeft;
+            btnNew.Location = new Point(26, 6);
+            btnNew.Name = "btnNew";
+            btnNew.Size = new Size(103, 45);
+            btnNew.TabIndex = 2;
+            btnNew.Text = "Novo";
+            btnNew.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnNew.UseVisualStyleBackColor = true;
+            btnNew.Click += btnNew_Click;
+            // 
+            // frmCity
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(955, 450);
+            Controls.Add(TabControl1);
+            Controls.Add(panel5);
+            Controls.Add(panel3);
+            Name = "frmCity";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Cidade";
+            Load += frmCity_Load;
+            TabControl1.ResumeLayout(false);
+            tpRegistration.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgCity).EndInit();
+            tpDatails.ResumeLayout(false);
+            tpDatails.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel7.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl TabControl1;
+        private TabPage tpRegistration;
+        private DataGridView dgCity;
+        private TabPage tpDatails;
+        private TextBox txtName;
+        private Label lblName;
+        private Label btnAcronym;
+        private Panel panel5;
+        private Panel panel2;
+        private CheckBox cbDesc;
+        private Button btnSearch;
+        private TextBox txtSearch;
+        private Panel panel1;
+        private TextBox txtSkip;
+        private Label label2;
+        private TextBox txtTake;
+        private Label label1;
+        private Button btnEnd;
+        private Button btnNext;
+        private Button btnPrevious;
+        private Button btnPrimary;
+        private Panel panel4;
+        private Button btnExit;
+        private Panel panel3;
+        private Panel panel7;
+        private Button btnCancel;
+        private Button btnSave;
+        private Panel panel6;
+        private Button btnDelete;
+        private Button btnUpdate;
+        private Button btnNew;
+        private ComboBox cbState;
+        private TextBox txtId;
+        private Label lblId;
+    }
+}
---
+++ FrontConfin/View/frmCity.cs
@@ -0,0 +1,271 @@
+﻿using FrontConfin.Helpers;
+using FrontConfin.Model;
+using FrontConfin.Service;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.DirectoryServices.ActiveDirectory;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrontConfin.View
+{
+    public partial class frmCity : Form
+    {
+        private PaginationResponse<City> pagination = null;
+        bool _verifiacation = false;
+
+        public frmCity()
+        {
+            InitializeComponent();
+            txtSkip.Text = "1";
+            txtTake.Text = "10";
+            UpdateData();
+        }
+
+        #region UpdateData
+        private async void UpdateData()
+        {
+            int.TryParse(txtSkip.Text, out int skip);
+            int.TryParse(txtTake.Text, out int take);
+
+            pagination = await CityService.GetPagination(txtSearch.Text, take: take, skip: skip,cbDesc.Checked);
+
+            dgCity.DataSource = pagination.Data;
+        }
+        #endregion
+
+        #region UpdateDatails
+        private void UpdateDatails()
+        {
+            City city = new City();
+
+            if (dgCity.SelectedRows.Count > 0)
+            {
+                city = (City)dgCity.SelectedRows[0].DataBoundItem;
+
+                txtId.Text = city.Id.ToString();
+                cbState.Text = city.StateSigla;
+                txtName.Text = city.Name;
+            }
+        }
+        #endregion
+
+        #region frmCity_Load
+        private async void frmCity_Load(object sender, EventArgs e)
+        {
+            List<State> list = new List<State>();
+            list = await StateService.GetAll();
+
+            cbState.DataSource = list.Select(s => s.Sigla).ToList();
+        }
+        #endregion
+
+        #region btnNew_click
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            _verifiacation = false;
+            TabControl1.SelectedTab = tpDatails;
+
+            cbState.Text = String.Empty;
+            txtName.Clear();
+
+            txtName.Focus();
+        }
+        #endregion
+
+        #region btnSave_click
+        private async void btnSave_Click(object sender, EventArgs e)
+        {
+            bool value = false;
+            try
+            {
+                City city = new City()
+                {
+                    StateSigla = cbState.Text ?? throw new ArgumentNullException("A cidade precisa de uma sigla de estado!"),
+                    Name = txtName.Text ?? throw new ArgumentNullException("Cidade precisa de um nome!"),
+                };
+
+                if (city is null)
+                {
+                    throw new NullReferenceException("Cidade não pode ser nulo");
+                }
+
+                if (_verifiacation)
+                {
+                    city.Id = Guid.Parse(txtId.Text.ToString());
+                    value = await CityService.Put(city);
+                }
+                else
+                {
+                    value = await CityService.Post(city);
+                }
+
+
+                if (value)
+                {
+                    TabControl1.SelectedTab = tpRegistration;
+                    UpdateData();
+                }
+            }
+            catch (ArgumentNullException ae)
+            {
+                MessageBox.Show(ae.Message);
+            }
+            catch (NullReferenceException ne)
+            {
+                MessageBox.Show(ne.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion
+
+        #region dgCity_SelectionChanged
+        private void dgCity_SelectionChanged(object sender, EventArgs e)
+        {
+            UpdateDatails();
+        }
+        #endregion
+
+        #region btnUpdate_click
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            _verifiacation = true;
+            txtId.Enabled = false;
+            TabControl1.SelectedTab = tpDatails;
+        }
+        #endregion
+
+        #region btnDelete_click
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Guid.TryParse(txtId.Text, out Guid id);
+                bool value = await CityService.Delete(id);
+
+                if (value)
+                {
+                    TabControl1.SelectedTab = tpRegistration;
+                }
+            }
+            catch (ArgumentNullException ae)
+            {
+                MessageBox.Show(ae.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+        #endregion
+
+        #region Exit
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+
+        #region btnSearch_Click
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            UpdateData();
+        }
+        #endregion
+
+        #region frmCity_KeyDown
+        private void frmCity_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnSave_Click(sender, e);
+            }
+        }
+        #endregion
+
+        #region btnPrimaryKey_Click
+        private void btnPrimary_Click(object sender, EventArgs e)
+        {
+            if (pagination.Skip > 1)
+            {
+                txtSkip.Text = "1";
+                UpdateData();
+            }
+            else
+            {
+                MessageBox.Show("Impossivel voltar para a primeira página");
+            }
+        }
+        #endregion
+
+        #region btnPrevious_click
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (pagination.Skip > 1)
+            {
+                pagination.Skip--;
+                txtSkip.Text = pagination.Skip.ToString();
+                UpdateData();
+            }
+            else
+            {
+                MessageBox.Show("Impossivel ir para a página 0");
+            }
+        }
+        #endregion
+
+        #region btnNext_click
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            decimal page = pagination.AllLine / pagination.Take;
+            int amountPage = (int)Math.Ceiling(page);
+
+            if (pagination.Skip < amountPage)
+            {
+                pagination.Skip++;
+                txtSkip.Text = pagination.Skip.ToString();
+                UpdateData();
+            }
+            else
+            {
+                MessageBox.Show("Impossivel ir para a proxima página");
+            }
+        }
+        #endregion
+
+        #region btnEnd_Click
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            decimal page = pagination.AllLine / pagination.Take;
+            int amountPage = (int)Math.Ceiling(page);
+
+            if (pagination.Skip < amountPage)
+            {
+                txtSkip.Text = pagination.Skip.ToString();
+                UpdateData();
+            }
+            else
+            {
+                MessageBox.Show("Não há proxima página");
+            }
+        }
+        #endregion
+
+        #region btnCancel_Click
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            TabControl1.SelectedTab = tpRegistration;
+            UpdateData();
+        }
+        #endregion
+    }
+}
